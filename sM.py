3639f1d8c0970534ff1d96d540afad1139476676
Athkalan al-Himyari <35337219+Al-Himyari@users.noreply.github.com>
2020-08-31 16:13:58 +0200

    Update sM.py

--- sM.py
+++ sM.py
@@ -2,4 +2,6 @@ import requests
 import logs
 def sendMessage(message1):
  requests.get("https://api.telegram.org/bot" + logs.Token + "/sendMessage?chat_id=" + logs.chat_id + "&text=" + message1)
- 
+
+def is_num(num):
+ isinstance(int(num), int)
